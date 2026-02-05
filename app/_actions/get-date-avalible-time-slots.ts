87d6d0ef793cc8a9caf7fd291eed7e0b8d0f8ddd
JuliaSTDev <souza.julia40@yahoo.com.br>
2026-02-05 16:57:29 -0300

    feat: ensure only available times are rendered on booking

---
+++ app/_actions/get-date-avalible-time-slots.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import { actionClient } from "@/lib/action-client";
+import { prisma } from "@/lib/prisma";
+import z, { date } from "zod";
+import { endOfDay, startOfDay } from "date-fns";
+import { format } from "date-fns";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { returnValidationErrors } from "next-safe-action";
+
+const inputSchema = z.object({
+  barbershopId: z.string(),
+  date: z.date(),
+});
+
+const TIME_SLOTS = [
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+];
+
+export const getDateAvalibleTimeSlots = actionClient
+  .inputSchema(inputSchema)
+  .action(async ({ parsedInput: { barbershopId, date } }) => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    if(session?.user) {
+        returnValidationErrors(inputSchema, {
+            _errors: ["unauthorized"],
+        });
+    }
+    const bookings = await prisma.booking.findMany({
+      where: {
+        barbershopId: barbershopId,
+        date: {
+          gte: startOfDay(date),
+          lte: endOfDay(date),
+        },
+      },
+    });
+
+    const ocupiedSlots = bookings.map((booking) =>
+      format(booking.date, "HH:mm"),
+    );
+    const avalibleTimeSlots = TIME_SLOTS.filter(
+      (slot) => !ocupiedSlots.includes(slot),
+    );
+    return avalibleTimeSlots;
+  });
